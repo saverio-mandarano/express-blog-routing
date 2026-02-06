7ecc9d06c8230e38e1e3a79bd7bf0b200e62fb9e
saverio mandarano <saverio.mandarano1@gmail.com>
2026-02-06 19:25:36 +0100

    add  file di routing, con rotte per opeazioni CRUD, per l'entità post.

---
+++ routers/posts.js
@@ -0,0 +1,32 @@
+// import Express
+const express = require("express");
+// creo variabile router il cui valore sarà un'istanza di express.Router()
+const router = express.Router();
+
+// index: lista dei post
+router.get("/", function (req, res) {
+  res.send("Lista dei post");
+});
+// show: dettagli post
+router.get("/:id", function (req, res) {
+  res.send("Dettagli del post con id: " + req.params.id);
+});
+// store: creazione nuovo post
+router.post("/", function (req, res) {
+  res.send("Creazione nuovo post");
+});
+// update: modifica integrale del post
+router.put("/:id", function (req, res) {
+  res.send("Modifica integrale del post con id: " + req.params.id);
+});
+// modify: modifica parziale del post
+router.patch("/:id", function (req, res) {
+  res.send("Modifica parziale del post con id: " + req.params.id);
+});
+// destroy: cancellazione del post
+router.delete("/:id", function (req, res) {
+  res.send("Eliminazione del post con id: " + req.params.id);
+});
+
+// esporto l'istanza di router
+module.exports = router;
